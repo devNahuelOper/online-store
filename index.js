04c3ac6ebb1b912eef4ef7921e602b18ac0d6210
“nahuel” <n.gorosito@yahoo.com>
2021-04-12 15:05:23 -0400

    Build Product model

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require("express");
+// const express = require("express");
 
-const app = express();
+const app = require("./server/server");
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+// app.listen(port, () => console.log(`Server listening on port ${port}`));
