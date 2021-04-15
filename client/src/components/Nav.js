c500ced0fa732e523c9c58732005567b03f58f58
“nahuel” <n.gorosito@yahoo.com>
2021-04-15 14:15:13 -0400

    Style NavBar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,6 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import {
+  MuiThemeProvider,
+  createMuiTheme,
+  makeStyles,
+} from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import LogoutButton from "./common/buttons/LogoutButton";
 import { Query } from "react-apollo";
@@ -10,43 +15,76 @@ import { ApolloConsumer } from "react-apollo";
 import Queries from "../graphql/queries";
 const { IS_LOGGED_IN } = Queries;
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    border: "1px solid #3506ef87",
+    boxShadow:
+      "0px 0px 4px 2px inset #9d39b2, 0px -2px 2px 4px #9d39b2b3, 0px -3px 4px 2px #010da4c9",
+  },
+  title: {
+    flexGrow: 1,
+    color: "#fff",
+    fontSize: `${1.5}rem`,
+    "&:hover": {
+      textShadow: `${2}px ${2}px #726d6d80`,
+    },
+  },
+  toolBar: {
+    display: "flex",
+    justifyContent: "space-around",
+  },
+}));
+
 const Nav = (props) => {
+  const classes = useStyles();
+
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#363636",
+      },
+    },
+  });
+
   return (
     <ApolloConsumer>
       {(client) => (
         <Query query={IS_LOGGED_IN}>
           {({ data }) => {
             return (
-              <AppBar position="static">
-                <Toolbar component="nav" className="nav">
-                  <Link to="/">
-                    <Typography variant="h5" className="nav-title">
-                      Products
-                    </Typography>
-                  </Link>
-                  {data.isLoggedIn ? (
-                    <LogoutButton
-                      onClick={(e) => {
-                        e.preventDefault();
-                        localStorage.removeItem("auth-token");
-                        client.writeData({ data: { isLoggedIn: false } });
-                        props.history && props.history.push("/");
-                      }}
-                    />
-                  ) : (
-                    <Link to="/login">
+              <MuiThemeProvider theme={theme}>
+                <AppBar position="static" className={classes.root}>
+                  <Toolbar component="nav" className="nav">
+                    <Link to="/">
                       <Typography variant="h5" className="nav-title">
-                        Login
+                        Products
                       </Typography>
                     </Link>
-                  )}
-                  <Link to="/register">
-                    <Typography variant="h5" className="nav-title">
-                      Register
-                    </Typography>
-                  </Link>
-                </Toolbar>
-              </AppBar>
+                    {data.isLoggedIn ? (
+                      <LogoutButton
+                        onClick={(e) => {
+                          e.preventDefault();
+                          localStorage.removeItem("auth-token");
+                          client.writeData({ data: { isLoggedIn: false } });
+                          props.history && props.history.push("/");
+                        }}
+                      />
+                    ) : (
+                      <Link to="/login">
+                        <Typography variant="h5" className="nav-title">
+                          Login
+                        </Typography>
+                      </Link>
+                    )}
+                    <Link to="/register">
+                      <Typography variant="h5" className="nav-title">
+                        Register
+                      </Typography>
+                    </Link>
+                  </Toolbar>
+                </AppBar>
+              </MuiThemeProvider>
             );
           }}
         </Query>
