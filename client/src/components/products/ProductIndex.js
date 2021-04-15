fbe1d7a647e31289030ab0c967b6c0316f90454b
“nahuel” <n.gorosito@yahoo.com>
2021-04-15 15:05:59 -0400

    Make FetchProduct gql query

--- client/src/components/products/ProductIndex.js
+++ client/src/components/products/ProductIndex.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Query } from "react-apollo";
+import { Link } from "react-router-dom";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Typography from "@material-ui/core/Typography";
+// import "./products.css";
 
 import Queries from "../../graphql/queries";
 const { FETCH_PRODUCTS } = Queries;
@@ -13,12 +15,14 @@ const ProductIndex = () => {
       {({ loading, error, data }) => {
         if (loading) return "Loading...";
         if (error) return `Error! ${error.message}`;
-
+        console.log(data);
         return (
-          <List>
+          <List className="product-index">
             {data.products.map((product) => (
-              <ListItem key={product._id}>
-                <Typography variant="h6">{product.name}</Typography>
+              <ListItem key={product._id} className="product">
+                <Link to={`/products/${product._id}`}>
+                  <Typography variant="h6">{product.name}</Typography>
+                </Link>
               </ListItem>
             ))}
           </List>
@@ -26,6 +30,6 @@ const ProductIndex = () => {
       }}
     </Query>
   );
-}
+};
 
 export default ProductIndex;
--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -2,10 +2,30 @@ import gql from "graphql-tag";
 
 const Queries = {
   FETCH_PRODUCTS: gql`
-    {
+    query FetchProducts{
       products {
         _id
         name
+        description
+        cost
+        category {
+          _id
+          name
+        }
+      }
+    }
+  `,
+  FETCH_PRODUCT: gql`
+    query FetchProduct($id: ID!) {
+      product(_id: $id) {
+        _id
+        name
+        description
+        cost
+        category {
+          _id
+          name
+        }
       }
     }
   `,
