76b3720ca1178725cb765d87c278a0138b7e8acd
“nahuel” <n.gorosito@yahoo.com>
2021-04-24 13:21:54 -0400

    Build EditTools component and add to NameDetail

---
+++ client/src/components/common/EditTools.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { IconContext } from "react-icons";
+import { FaPencilAlt, FaPlus, FaMinus } from "react-icons/fa";
+
+const EditTools = ({ onClick, title, icon = "pencil"}) => {
+  return (
+    <div className={`edit-tools ${icon}-tool`} onClick={onClick} title={title}>
+      <IconContext.Provider value={{ className: `custom-icon ${icon}`}}>
+        {icon == "pencil" && <FaPencilAlt />}
+      </IconContext.Provider>
+    </div>
+  )
+}
+
+export default EditTools;
--- client/src/components/products/Detail/NameDetail.js
+++ client/src/components/products/Detail/NameDetail.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { ClickAwayListener } from "@material-ui/core";
 import { Mutation } from "react-apollo";
 import NameField from "../../common/fields/NameField";
+import EditTools from "../../common/EditTools";
 
 import Mutations from "../../../graphql/mutations";
 const { UPDATE_PRODUCT_NAME } = Mutations;
@@ -26,11 +27,27 @@ const NameDetail = (props) => {
                     },
                   }).then(() => handleEdit(false));
                 }}
-              ></form>
+              >
+                <NameField
+                  name={name}
+                  entity="Product"
+                  onChange={(e) => update(e.target.value)}
+                />
+                <button className="update-btn" type="submit">
+                  Update Name
+                </button>
+              </form>
             </div>
           </ClickAwayListener>
         )}
       </Mutation>
+    );
+  } else {
+    return (
+      <div>
+        <h1 className="product-name">{name}</h1>
+        <EditTools onClick={() => handleEdit(true)} title="Edit Product Name"/>
+      </div>
     );
   }
 };
