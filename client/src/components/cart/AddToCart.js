e7c75ae038f477143fd453485843cfcdbc7309c8
“nahuel” <n.gorosito@yahoo.com>
2021-05-02 11:02:23 -0400

    Implement addToCart functionality on ProductDetail

--- client/src/components/cart/AddToCart.js
+++ client/src/components/cart/AddToCart.js
@@ -1,18 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import RemoveShoppingCartIcon from "@material-ui/icons/RemoveShoppingCart";
-import { Query, Mutation } from "react-apollo";
+import { Query, ApolloConsumer } from "react-apollo";
 
 import Queries from "../../graphql/queries";
 const { FETCH_CART_ITEMS } = Queries;
 
-const AddToCart = ({ _id, name, cost }) => {
+const AddToCart = (props) => {
+  const addItemToCart = (e, cache) => {
+    e.preventDefault();
 
-  const addItemToCart = (cache, { data }) => {
-    const addedItem = { _id, cost, name };
-    console.log('cache', cache);
-    console.log('item', addedItem);
-    console.log('data', data)
+    const addedItem = { _id: props._id, name: props.name, cost: props.cost };
     let cart;
 
     try {
@@ -21,8 +19,6 @@ const AddToCart = ({ _id, name, cost }) => {
       return;
     }
 
-    // inCart = cart?.cart.find((item) => item._id == _id);
-
     if (cart) {
       let cartArray = cart.cart;
       cache.writeQuery({
@@ -30,59 +26,40 @@ const AddToCart = ({ _id, name, cost }) => {
         data: { cart: [...cartArray, addedItem] },
       });
     }
-
-    console.log(cart);
   };
 
   return (
-    <Query query={FETCH_CART_ITEMS}>
-      {({ loading, error, data }) => {
-        if (loading) return "Loading...";
-        if (error) return `Error! ${error.message}`;
-        console.log(data.cart);
+    <ApolloConsumer>
+      {(cache) => (
+        <Query query={FETCH_CART_ITEMS} variables={{ productId: props._id }}>
+          {({ loading, error, data }) => {
+            if (loading) return "Loading...";
+            if (error) return `Error! ${error.message}`;
 
-        const inCart = data.cart.find((item) => item._id == _id);
-        console.log(inCart);
+            const inCart = data.cart.some((item) => item._id == props._id);
 
-        if (!inCart) {
-          return (
-            <div>
-              <AddShoppingCartIcon
-                className="add-to-cart"
-                title={`Add ${name} to Cart`}
-                onClick={(cache, { _id, name, cost }) =>
-                  addItemToCart(cache, { _id, name, cost })
-                }
-              />
-            </div>
-          );
-        } else {
-          return (
-            <div>
-              <RemoveShoppingCartIcon />
-            </div>
-          );
-        }
-      }}
-    </Query>
+            if (!inCart) {
+              return (
+                <div>
+                  <AddShoppingCartIcon
+                    className="add-to-cart"
+                    title={`Add ${props.name} to Cart`}
+                    onClick={(e) => addItemToCart(e, cache)}
+                  />
+                </div>
+              );
+            } else {
+              return (
+                <div>
+                  <RemoveShoppingCartIcon />
+                </div>
+              );
+            }
+          }}
+        </Query>
+      )}
+    </ApolloConsumer>
   );
-  // if (!inCart) {
-  //   return (
-  //     <div>
-  //       <AddShoppingCartIcon
-  //         className="add-to-cart"
-  //         title={`Add ${name} to Cart`}
-  //         onClick={(cache) => addItemToCart(cache)}
-  //       />
-  //     </div>
-  //   );
-  // } else {
-  //   return (
-  //     <div>
-  //       <RemoveShoppingCartIcon />
-  //     </div>
-  //   );
-  // }
 };
 
 export default AddToCart;
