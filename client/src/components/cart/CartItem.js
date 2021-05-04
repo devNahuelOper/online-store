ab4c870350a860755046ee6f6c846f6454f2c779
“nahuel” <n.gorosito@yahoo.com>
2021-05-04 08:13:23 -0400

    Implement CartItem component

--- client/src/components/cart/CartItem.js
+++ client/src/components/cart/CartItem.js
@@ -1,5 +1,27 @@
 import React from "react";
-import { Query, ApolloConsumer } from "react-apollo";
+import { Query } from "react-apollo";
 
 import Queries from "../../graphql/queries";
-const { FETCH_CART_ITEMS } = Queries;
+const { FETCH_PRODUCT } = Queries;
+
+const CartItem = ({ _id }) => {
+  return (
+    <Query query={FETCH_PRODUCT} variables={{ productId: _id }}>
+      {({ loading, error, data }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>{error.message}</p>;
+
+        const { name, description, cost } = data.product;
+        return (
+          <div>
+            <p>Name: {name}</p>
+            <p>Description: {description}</p>
+            <p>Price: ${cost}</p>
+          </div>
+        )
+      }}
+    </Query>
+  )
+}
+
+export default CartItem;
