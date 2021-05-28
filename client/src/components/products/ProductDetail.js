4198213e5b4c66c9fc5391cebe41dc9ff1f98b76
“nahuel” <n.gorosito@yahoo.com>
2021-05-28 08:27:29 -0400

    Find which products have category linked

--- client/src/components/products/ProductDetail.js
+++ client/src/components/products/ProductDetail.js
@@ -18,7 +18,8 @@ const ProductDetail = (props) => {
         if (loading) return "Loading...";
         if (error) return `Error! ${error.message}`;
 
-        const { _id, name, description, cost, image } = data.product;
+        const { _id, name, description, cost, image, category } = data.product;
+        console.log(category);
         return (
           <div className="product-detail">
             <DeleteProduct _id={_id} name={name} />
