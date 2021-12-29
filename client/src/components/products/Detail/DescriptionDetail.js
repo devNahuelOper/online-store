e190368b80f40de1cb09f258da95279b30db0ff0
“nahuel” <n.gorosito@yahoo.com>
2021-12-29 08:29:29 -0500

    WIP

--- client/src/components/products/Detail/DescriptionDetail.js
+++ client/src/components/products/Detail/DescriptionDetail.js
@@ -4,6 +4,7 @@ import { ClickAwayListener } from "@material-ui/core";
 import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
 import EditTools from "../../common/EditTools";
 import Mutations from "../../../graphql/mutations";
+
 const { UPDATE_PRODUCT_DESCRIPTION } = Mutations;
 
 const DescriptionDetail = (props) => {
